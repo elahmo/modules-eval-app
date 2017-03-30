b230de8bf1a4937c609716686216d8a45b9e9d0c
multiks2200 <multiks2200@gmail.com>
2017-03-30 13:16:19 +0100

    small test

--- backendAPI/web.js
+++ backendAPI/web.js
@@ -41,7 +41,7 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hello dawg, this is api!' });
+	res.json({ message: 'hello dawg, this is backend api !' });
 });
 
 // REGISTER OUR ROUTES -------------------------------
